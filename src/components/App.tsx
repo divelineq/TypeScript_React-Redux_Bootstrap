a991358b19b800b9c86b3f2555e925a1e0e63802
Dmitry <divelineq@gmail.com>
2021-12-15 15:02:57 +0700

    commit

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -24,18 +24,18 @@ function App() {
 
   return (
     <div>
-      <Navbar bg='light' expand='lg'>
+      <Navbar bg='dark' expand='lg' variant='dark'>
         <Container>
           <Navbar.Brand href='#home'>React-Bootstrap</Navbar.Brand>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
             <Nav className='me-auto'>
-              <Nav.Link href='#home'>
+              <Nav.Link>
                 <Link className={a.start} to='/'>
                   Начало
                 </Link>
               </Nav.Link>
-              <Nav.Link href='#link'>
+              <Nav.Link>
                 <Link className={a.posts} to='/posts'>
                   Показать все посты
                 </Link>
--- src/components/StartPage.tsx
+++ src/components/StartPage.tsx
@@ -1,11 +1,49 @@
-import React from 'react'
-import s from './StartPage.module.css'
+import React, { useState } from 'react'
+import { Carousel } from 'react-bootstrap'
+import './StartPage.module.css'
 export default function StartPage() {
+  const [index, setIndex] = useState(0)
+  const handleSelect = (selectedIndex: any, e: any) => {
+    setIndex(selectedIndex)
+  }
+
   return (
-    <div>
-      <div className={s.div}>
-        <h1>Start Page</h1>
-      </div>
-    </div>
+    <Carousel activeIndex={index} onSelect={handleSelect}>
+      <Carousel.Item style={{ height: '792px' }}>
+        <img
+          className='d-block w-100'
+          src='https://mobimg.b-cdn.net/v3/fetch/53/53cbad6d18496d8e689ee4b170ec0712.jpeg'
+          alt='First slide'
+        />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item style={{ height: '792px' }}>
+        <img
+          className='d-block w-100'
+          src='https://mobimg.b-cdn.net/v3/fetch/53/53cbad6d18496d8e689ee4b170ec0712.jpeg'
+          alt='Second slide'
+        />
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item style={{ height: '792px' }}>
+        <img
+          className='d-block w-100'
+          src='https://mobimg.b-cdn.net/v3/fetch/53/53cbad6d18496d8e689ee4b170ec0712.jpeg'
+          alt='Third slide'
+        />
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
   )
 }
